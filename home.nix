de699a6ebccb9e6c56c582eb8c64ec5fec1e5d69
crayonnova <kaungminkhant.dev@gmail.com>
2026-04-19 12:15:34 +0700

    fix: cleanup

--- home.nix
+++ home.nix
@@ -222,20 +222,7 @@
       imagemagick
     ];
   };
-  # home.file = {
-  #   ".config/nvim" = {
-  #     # source = ~/dotfiles/nvim/.config/nvim;
-  #     source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/nvim/.config/nvim";
-  #     recursive = true;
-  #   };
-  #
-  #   ".config/starship.toml" = {
-  #     source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/starship/.config/starship.toml";
-  #   };
-  #   ".tmux.conf" = {
-  #     source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/tmux/.tmux.conf";
-  #   };
-  # };
+
   home.sessionVariables = {
     EDITOR = "nvim";
   };
