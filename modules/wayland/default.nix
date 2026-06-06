d9883066b0ce482c784a642ffc6461d7dfe4e53c
Edimilson-Miranda <edimilsonsmp.464@gmail.com>
2026-06-05 22:26:48 -0300

    conflido do sddm

--- modules/wayland/default.nix
+++ modules/wayland/default.nix
@@ -14,11 +14,6 @@
   config = lib.mkIf config.myconfig.features.desktop {
     # programs.niri.enable = true;
     # programs.niri.package = pkgs.niri;
-
-  programs.niri = {
-    enable = true;
-    package = pkgs.niri; # Ou use o input niri.packages.${pkgs.system}.niri
-  };
   
   };
 }
