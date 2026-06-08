fd5b302e225e4cc115718e450991e4b0328bb77e
Edimilson-Miranda <edimilsonsmp.464@gmail.com>
2026-06-08 18:25:35 -0300

    Adionando tema ao sddm

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
       # 1. Configuração do Sistema (NixOS)
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         inherit system;
-        extraSpecialArgs = {inherit inputs;};
+        specialArgs = {inherit inputs;};
         modules = [
           ./configuration.nix
           home-manager.nixosModules.home-manager
